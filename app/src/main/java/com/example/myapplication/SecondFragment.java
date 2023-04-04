a622ea5c717360f9b0a09b008ed08d41667a6996
Sokolova Mariia <mashasokolova7@gmail.com>
2023-04-04 10:04:07 +0300

    Добавила ListView на 2 фрагмент

--- app/src/main/java/com/example/myapplication/SecondFragment.java
+++ app/src/main/java/com/example/myapplication/SecondFragment.java
@@ -2,12 +2,18 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class SecondFragment extends Fragment {
@@ -18,6 +24,7 @@ public class SecondFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_second, container, false);
         button_1 = (Button) view.findViewById(R.id.button21);
         button_1.setOnClickListener(new View.OnClickListener() {
@@ -46,4 +53,19 @@ public class SecondFragment extends Fragment {
         return view;
     }
 
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        List<String> store_items = new ArrayList<String>();
+        for (int i = 1; i < 201; i++){
+            store_items.add("Коктейль " + i);
+        }
+
+        MyAdapter listViewAdapter = new MyAdapter(
+                getActivity(),R.layout.store_item, store_items);
+
+        ListView listView = view.findViewById(R.id.list_view);
+        listView.setAdapter(listViewAdapter);
+    }
 }
+
